f81a214d20880341edec990e4fd49e8d6df08398
MuhammadShahzaib-shah <102shahzaib@gmail.com>
2026-02-09 12:42:22 +0500

    new code added

---
+++ NumPy/Practice/1.py
@@ -0,0 +1,17 @@
+import numpy as np
+# arr = np.array([10, 20, 30, 40, 50, 60])
+# print(arr.dtype)
+# arr_float = arr.astype(float)
+# print(arr_float.dtype)
+# arr_str = arr.astype(str)
+# print(arr_str.dtype)
+
+# arr = np.arange(-20 , 20 , 2)
+# arr1 = arr.reshape(5,4)
+# print(arr1)
+# print(arr1.shape)
+# print(arr1.ndim)
+# print(arr1.size)
+
+arr =np.eye(6)
+print (arr)
